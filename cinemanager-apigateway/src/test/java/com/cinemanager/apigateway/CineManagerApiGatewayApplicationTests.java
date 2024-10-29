2544b71d45553e700f1ca7e6d8c62190270e3dd0
Jose Márquez <josemarqmontoro@hotmail.com>
2024-10-29 17:28:52 +0100

    add apigateway project

---
+++ cinemanager-apigateway/src/test/java/com/cinemanager/apigateway/CineManagerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemanager.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineManagerApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
