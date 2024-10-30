30f552e8420130d61251ac448f37c3a37605935e
Jose Márquez <josemarqmontoro@hotmail.com>
2024-10-30 16:20:54 +0100

    add user service project

---
+++ cinemanager-user-service/src/main/java/com/cinemanager/cinemanager_user_service/CineManagerUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cinemanager.cinemanager_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineManagerUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineManagerUserServiceApplication.class, args);
+	}
+
+}
